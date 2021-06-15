530a5597acef4c9d0e598e7fd7385cea18f2812d
joel <joelcrawford30@gmail.com>
2021-06-15 13:01:11 +0300

    Teleporting modal to another class

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,35 @@
 
   <p>Welcome.....</p>
   
-  <div v-if="showModal">
+  <!-- <div v-if="showModal"> -->
     <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/> -->
 
      <!-- <Modal theme="sale" @close="toggleModal"/> -->
 
      <!-- https://youtu.be/KM1U6DqZf8M?list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1 -->
+
+       <!-- <Modal theme="" @close="toggleModal"> -->
+
+
+<!-- passing a name slot -->
+         <!-- <template v-slot:links>
+           <a href="#">Sign Up</a>
+            <a href="#">More Info</a>
+         </template> -->
+
+<!-- passing a Default slot -->
+
+         <!-- <h1>Joel</h1>
+         <p>Grab your data</p>
+       </Modal>
+    </div> -->
+
+
+
+
+<!-- ============Teleporting it .modals the css class in index.html================-->
+      <teleport to=".modals" v-if="showModal">
+     <!-- https://youtu.be/KM1U6DqZf8M?list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&t=2505 -->
 
        <Modal theme="" @close="toggleModal">
 
@@ -24,7 +47,8 @@
          <h1>Joel</h1>
          <p>Grab your data</p>
        </Modal>
-    </div>
+    </teleport>
+
 
 
 
@@ -84,7 +108,7 @@ export default {
 </script>
 
 <style scoped>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
